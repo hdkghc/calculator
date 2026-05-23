eb4d88234fb31c30befa8af7cf159cb7b4c8eb60
hdkghc <peitongxin@outlook.com>
2026-05-23 19:55:20 +0800

    modified:   inc/cas/intg.hpp

--- inc/cas/intg.hpp
+++ inc/cas/intg.hpp
@@ -74,12 +74,12 @@ namespace CAS {
             }
         public:
             Intg() : f(0) {}
-            Intg(uint64_t x) : f(0) {
-                for(size_t i = 0; x; ++i) {
-                    sdg(i, x % 10);
-                    x /= 10;
-                }
-            }
+            // Intg(uint64_t x) : f(0) {
+            //     for(size_t i = 0; x; ++i) {
+            //         sdg(i, x % 10);
+            //         x /= 10;
+            //     }
+            // }
             Intg(int64_t x) {
                 if(x < 0) f = 1;
                 else f = 0;
@@ -160,7 +160,145 @@ namespace CAS {
             Intg operator-(Intg rhs) const {
                 return *this + (-rhs);
             } // operator-
-            
+            /** @name abs
+             *  @brief Get the absolute value of the integer
+             *  @return Absolute value of the integer
+            */
+            Intg abs(void) {
+                Intg ret = *this;
+                ret.f &= 0xFE;
+                return ret;
+            }
+            uint8_t operator[](size_t i) {
+                return (gdg(i) == (uint8_t)(-1)) ? 0 : gdg(i);
+            }
+        protected:
+            /** @name cmp
+             *  @brief Compare two integers
+             *  @param lhs Left-hand side integer
+             *  @param rhs Right-hand side integer
+             *  @return 1 if lhs > rhs, 0 if lhs < rhs, 2 if lhs == rhs
+             */
+            uint8_t cmp(Intg lhs, Intg rhs) const {
+                if(lhs.gl() > rhs.gl()) {
+                    return 1;
+                } else if(lhs.gl() < rhs.gl()) {
+                    return 0;
+                }
+                for(size_t i = lhs.gl() - 1; i >= 0; --i) {
+                    if(lhs[i] > rhs[i]) {
+                        return 1;
+                    } else if(lhs[i] < rhs[i]) {
+                        return 0;
+                    }
+                }
+                return 2;
+            }
+        public:
+            bool operator>(Intg rhs) {
+                return cmp(*this, rhs) == 1;
+            }
+            bool operator<(Intg rhs) {
+                return cmp(*this, rhs) == 0;
+            }
+            bool operator>=(Intg rhs) {
+                return cmp(*this, rhs) >= 1;
+            }
+            bool operator<=(Intg rhs) {
+                return cmp(*this, rhs) != 1;
+            }
+            bool operator==(Intg rhs) {
+                return cmp(*this, rhs) == 2;
+            }
+            bool operator!=(Intg rhs) {
+                return cmp(*this, rhs) != 2;
+            }
+        protected:
+            /** @name digmul
+             *  @brief Multiply two single digits (0-9) and return the result as a byte (0-99)
+             *  @param a First digit (0-9)
+             *  @param b Second digit (0-9)
+             *  @return The product of the two digits as a byte (0-99)
+             *  @usage uint8_t result = digmul(7, 8); // result will be 0x56 in hexadecimal
+             */
+            uint8_t digmul(uint8_t a, uint8_t b) {
+                return makebyte((a * b) / 10, (a * b) % 10);
+            }
+            /** @name makebyte
+             *  @brief Combine two single digits (0-9) into a byte (0-99)
+             *  @param hi High digit (0-9)
+             *  @param lo Low digit (0-9)
+             *  @return The byte created from the two digits
+             */
+            uint8_t makebyte(uint8_t hi, uint8_t lo) {
+                return (hi << 4) | (lo & 0x0F);
+            }
+            /** @name gethi
+             *  @brief Get the high digit of a byte
+             */
+            uint8_t gethi(uint8_t b) {
+                return b >> 4;
+            }
+            /** @name getlo
+             *  @brief Get the low digit of a byte
+             */
+            uint8_t getlo(uint8_t b) {
+                return b & 0x0F;
+            }
+            /** @name muldig
+             *  @brief Multiply the integer by a single digit
+             *  @param lhs The integer to be multiplied
+             *  @param rhs The single digit multiplier (0-9)
+             *  @return The product of the integer and the single digit
+             */
+            Intg muldig(Intg lhs, uint8_t rhs) {
+                uint8_t c = 0;
+                size_t l = lhs.gl();
+                for(size_t i = 0; i < l; ++i) {
+                    uint8_t tmp = lhs[i] * rhs + c;
+                    if(tmp > 9) {
+                        lhs.sdg(i, tmp % 10);
+                        c = tmp / 10;
+                    } else {
+                        lhs.sdg(i, tmp);
+                        c = 0;
+                    }
+                }
+                if(c) {
+                    lhs.sdg(l, c);
+                }
+                return lhs;
+            }
+            /** @name append0
+             *  @brief Append n zeroes to the integer (multiply by 10^n)
+             *  @param n Number of zeroes to append
+             *  @return The integer after appending zeroes
+             */
+            Intg append0(size_t n) {
+                if(gl() == 0) {
+                    return *this;
+                }
+                if(n & 1) {
+                    for(size_t i = gl() - 1; i >= 0; --i) {
+                        sdg(i + n, this->operator[](i));
+                        sdg(i, 0);
+                    }
+                } else {
+                    i.insert(i.begin(), (n + 1) >> 1, 0);
+                }
+                return *this;
+            }
+        public:
+            Intg operator*(Intg rhs) {
+                Intg ret;
+                for(size_t i = 0; i < this->gl(); ++i) {
+                    Intg tmp = muldig(rhs, this->operator[](i));
+                    tmp.append0(i);
+                    ret = ret + tmp;
+                }
+                ret.f = (this->f ^ rhs.f) & 0x01;
+                return ret;
+            }
     };
 }
 
